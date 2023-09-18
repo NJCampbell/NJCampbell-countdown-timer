a02b11603749f7db38be69be20a9415c0207088b
Nancy J Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-09-18 17:11:22 +0000

    added component, began build

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,10 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { CountdownTimer } from "./countdownTimer.jsx";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>Countdown to Con</h1>
+		<CountdownTimer />
 	</div>
 );
